12dd76f0e869d572f6c16eba47b360569aa54dc4
lilianacandrea <gogonicipici@gmail.com>
2018-07-05 13:09:14 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,26 +3,11 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-// created a client side script that connects to the server and as soon as it connects it emits this create email event.
-  // socket.emit('createEmail', {
-  //   to: 'jen@example.com',
-  //   text: 'hey. This is Lili.'
-  // });
-
-  socket.emit('createMessage', {
-    from: 'tananana@',
-    text: 'Yup, that works for me.'
-  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
-
-// socket.on('newEmail', function (email) {
-//   console.log('New email', email);
-// });
 
 socket.on('newMessage', function (message) {
   console.log('New message', message);
--- server/server.js
+++ server/server.js
@@ -15,30 +15,16 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-// Connection event: this lets you listen for a new connection meaning tat a client connected to the server and it lets you do something when that connection comes.
-// In order to do something you provide a callaback fuction as the second argument and this callback func. is going to get called with a socket.
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newEmail', {
-  //   from: 'mike@example.com',
-  //   text: 'Hey. What is going on.',
-  //   createdAt: 123
-  // });
-
-  // socket.on('createEmail', (newEmail) => {
-  //   console.log('createEmail', newEmail);
-  // });
-
-  socket.emit('newMessage', {
-    from: 'john',
-    text: 'see you then',
-    createdAt: 123
-  });
-
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', (socket) => {
